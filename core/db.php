3f72db7c68c26b50b79770159c2fa298c88c2af4
Roman <rumsnownsk@gmail.com>
2020-01-22 12:11:59 +0700

    test1

--- core/db.php
+++ core/db.php
@@ -7,7 +7,7 @@ $m = new Manager;
 $m->addConnection([
     'driver' => 'mysql',
     'host' => 'localhost',
-    'database' => 'short-url-test1234',
+    'database' => 'short-url',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
